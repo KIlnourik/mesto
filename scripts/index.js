d6cb6ec14c194a6a5b3a829e940fcea661d5a9d0
KIlnur <kilnur1988@rambler.ru>
2021-11-14 21:58:29 +0500

    fix правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,26 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
-const nameInput = popup.querySelector('.popup__input_name');
-const jobInput = popup.querySelector('.popup__input_job');
+const nameInput = popup.querySelector('#name');
+const jobInput = popup.querySelector('#job');
 const popupForm = popup.querySelector('.popup__form');
-
+//функция открытия попапа
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
+//функция закрытия попапа
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-editButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-
+//функция передачи введенных данных из формы попапа в соответствующие места на сайте
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let nameInputValue = nameInput.value;
-  let jobInputValue = jobInput.value;
-  profileName.textContent = nameInputValue;
-  profileJob.textContent = jobInputValue;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
   closePopup();
 }
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
